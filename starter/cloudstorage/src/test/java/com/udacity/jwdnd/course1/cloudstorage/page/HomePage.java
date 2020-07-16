4c550d5164b8a19476509f9ac79efc4ae770bd87
Piotr Głębocki <piotr.glebocki@vimn.com>
2020-07-16 21:43:45 +0200

    implement tests for access pages

---
+++ starter/cloudstorage/src/test/java/com/udacity/jwdnd/course1/cloudstorage/page/HomePage.java
@@ -0,0 +1,20 @@
+package com.udacity.jwdnd.course1.cloudstorage.page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+
+    @FindBy(css = "#logoutButton")
+    private WebElement logout;
+
+    public HomePage(WebDriver webDriver) {
+        PageFactory.initElements(webDriver, this);
+    }
+
+    public void logout() {
+        logout.click();
+    }
+}
